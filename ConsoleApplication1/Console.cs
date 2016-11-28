452e4e9410efdea64ff902077ac07ff73bb5378a
adaml <adaml@DAMO-WS01>
2016-11-27 22:52:30 -0500

    test

---
+++ ConsoleApplication1/Console.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Console
+    {
+        internal static void WriteLine(int testInt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int testInt = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                testInt += 1;
+            }
+            Console.WriteLine(testInt);
         }
     }
 }
